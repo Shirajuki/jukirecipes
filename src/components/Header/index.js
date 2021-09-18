1d206d8e79bf322476a22a2876cfe75942ea2659
Shirajuki <shirajuki00@gmail.com>
2021-09-18 21:14:13 +0200

    Removes unused variables

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import SearchBar from "../SearchBar";
 import ThemeChangeButton from "../ThemeChangeButton";
 import { useStateValue } from "../../state";
